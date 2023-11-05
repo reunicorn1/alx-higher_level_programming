bd8a3efe91038c01e4307888712f01d7f9ff69ad
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-11-05 13:13:12 +0400

    print a list of integers in reverse

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
-
-my_list = [0, 4, 5, 6, 7]
+my_list = None
 print_reversed_list_integer(my_list)
