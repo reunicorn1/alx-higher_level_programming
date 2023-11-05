eb46e85fcaef8f46f1a26f0634fabf0de0f39d81
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-11-05 15:00:29 +0400

    lists of lists = matrix mod 3

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         if len(row) != 0:
             for i in range(len(row)):
-                print("{:d}".format(row[i]), end=" " if i + 1 != len(row) else "\n")
+                print("{:d}".format(row[i]), end=" "
+                      if i + 1 != len(row) else "\n")
         else:
             print()
