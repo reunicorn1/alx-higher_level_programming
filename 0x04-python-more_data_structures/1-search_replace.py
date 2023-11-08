b73c93e081b56dcc4a498a6bd08626f0e151df6a
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-11-08 22:08:24 +0400

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if len(my_list):
+        new_list = list(map(lambda x: replace if x == search
+                            else x, my_list))
+        return new_list
