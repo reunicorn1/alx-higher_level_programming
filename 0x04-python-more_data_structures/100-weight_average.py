4934105e593cd56aba5a54dbe316549dff80b337
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-11-09 11:41:10 +0400

    weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,3 +4,5 @@ def weight_average(my_list=[]):
         total = sum(value_1 * value_2 for value_1, value_2 in my_list)
         sub_total = sum(value_2 for _, value_2 in my_list)
         return float(total / sub_total)
+    else:
+        return 0
