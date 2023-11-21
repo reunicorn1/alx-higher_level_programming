38e783f7b3a2e85d568af9f4a3230bdd5b48ab5c
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-11-21 23:48:20 +0400

    my first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
+"""Defining a class Square"""
+
+
 class Square:
     """
     This is a class Square which is currently empty.
     No Attributes or methods are created for this class yet
     """
-    pass
+    def square
