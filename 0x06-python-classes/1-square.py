bc6e37c4d5a4250ca5111e364b725d3a1c35dfc6
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-11-22 00:11:43 +0400

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is a definition of the class Square"""
+
+
+class Square:
+    """This class is defining a Square.
+
+     No public attributes are defined currently
+
+     The __init__method here is initilizing no public attributes.
+
+     Args:
+        size (int): the value which indicate the size of the square.
+
+     """
+    def __init__(self, size):
+        self.__size = size
