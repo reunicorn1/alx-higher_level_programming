eb006e57fb6283a35f627f168736ccf87b826262
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-11-22 20:31:43 +0400

    bytecode python #5

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import math
+"""This is the MagicClass slass"""
+
+
+class MagicClass:
+    """This function appears to take a radius and calculate the
+    area ans circumference of a circle.
+
+    There is no public parameters that were defined for this class.
+
+    The __init__ method checks for the value of radius if its a valid
+    integer or a float and raises an exception if it's otherwise.
+
+    """
+    def __init__(self, radius):
+        self._MagicClass__radius = 0
+        if type(radius) != int and type(radius) != float:
+            raise TypeError('radius must be a number')
+        self._MagicClass__radius = radius
+        return None
+
+    def area(self):
+        """This method calculates the area of a circle.
+
+        Returns:
+           The result of the sum of pi * radius ^ 2.
+        """
+        return (self._MagicClass__radius ** 2) * math.pi
+
+    def circumference(self):
+        """This method calculates the circumference of a circle.
+
+        Returns:
+           The result of the sum of 2 * pi * radius.
+        """
+        return (2 * math.pi) * self._MagicClass__radius
+
+import dis
+
+dis.dis(MagicClass.__init__)
+dis.dis(MagicClass.area)
+dis.dis(MagicClass.circumference)
