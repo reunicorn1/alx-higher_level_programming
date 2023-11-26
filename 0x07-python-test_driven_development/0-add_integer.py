120a3f439f50ace33e7b2bff9cd1eeadf504c54c
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-11-26 21:35:08 +0400

    integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+This is the '0-add_integer' module.
+
+This module supplies one function, add_integer. For example,
+
+>>> add_integer(1, 2)
+3
+"""
+
+def add_integer(a, b=98):
+    """Returns the addition of a and b, which must be integers or floats.
+
+    >>> add_integer(100, -2)
+    98
+
+    >>> add_integer(2)
+    100
+
+    >>> add_integer(100.3, -2)
+    98
+
+    If a and b are not floats or integers, a TypeError exception is raised.
+
+    >>> add_integer(4, 'School')
+    Traceback (most recent call last):
+        ...
+    TypeError: b must be an integer
+
+    >>> add_integer(None)
+    Traceback (most recent call last):
+        ...
+    TypeError: a must be an integer
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    elif not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,17 @@
+# 0x07. Python - Test-driven development
+
+Test-driven development (TDD) is a software development process that relies on the repetition of a very short development cycle. In Python, as in other languages, the steps are as follows:
+Write a test for the smallest possible increment of functionality.
+Run the test, which should fail because the functionality isn't implemented yet.
+Write just enough code to make the test pass.
+Refactor the code, making it as simple and clear as possible without breaking the functionality (the test should still pass).
+This cycle is repeated for each new functionality until the software is complete. The idea is that by writing tests first, you ensure that your code is always testable and that every piece of functionality is tested.
+
+# Files
+
+### 0. Integers addition
+### 1. Divide a matrix
+### 2. Say my name
+### 3. Print square
+### 4. Text indentation
+### 5. Max integer - Unittest
