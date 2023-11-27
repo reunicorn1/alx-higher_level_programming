1b55c08ab0b8daeac8d4245f2e5fedfcbd360f4e
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-11-27 07:27:58 +0400

    say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+
+"""
+This is the '3-say_my_name' module.
+
+This module supplies one function, say_my_name. For example,
+
+>>> say_my_name("John", "Smith")
+My name is John Smith
+"""
+
+def say_my_name(first_name, last_name=""):
+    """Prints the string "My name is" followed by first name and last name.
+
+    >>> say_my_name("Walter", "White")
+    My name is Walter White
+
+    >>> say_my_name("Bob")
+    My name is Bob 
+
+    Parameters:
+       first_name (str): The first name to be added
+       last_name (str): The last name to be added
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    elif not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print("My name is {} {}".format(first_name, last_name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
