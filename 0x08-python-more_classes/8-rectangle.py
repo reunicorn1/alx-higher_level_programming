ef74ce6a6ce0fb2c6227023a71a4a6b1d5b7ed9b
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-11-28 05:54:45 +0400

    compare rectangles

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+"""
+This is '0-rectangle' module.
+
+This module suuplies one class, Rectangle.
+"""
+
+
+class Rectangle:
+    """This class creates an empty rectangle.
+
+    No attributes are publically defined yet.
+    The __init__ method which works as the constructor for the
+    Rectangle instances by providing a height and a width.
+
+    Attributes:
+       width (int): is a prive instance attribute
+       height (int): is a private instance attribute
+
+    """
+
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """width property is checked and provided using both getter
+        and setter functions.
+        The value in the setter function is checked if it's an integer
+        and not less than zero. If otherwise, an exception is raised.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """The height function has both a getter and a setterr.
+
+        The setter checks for the value if it's an int and above 0.
+        If not it raises an excption error.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
+    def area(self):
+        """This function calculates the area of rectangle using
+        it's attributes which are height and width.
+
+        Returns:
+           The area of a rectangle <A = l*w>.
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """This functin calculates the perimeter of a rectangle using
+        it's attributes which are the height and the width.
+
+        Returns:
+           the perimeter of a rectangle <P = 2(l+w)>
+        """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        """This function provides the ouput that should Rectangle
+        class provide to the end user when it's printed, as well
+        as it makes the object readable.
+        """
+        string = []
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        for h in range(self.__height):
+            string.append(str(self.print_symbol) * self.__width)
+        return '\n'.join(string)
+
+    def __repr__(self):
+        """The repr function is used to provide the internal
+        of an object and creates code the reproduces the object.
+        """
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        elif not isinstance(rect_2, Rectangle):
+            raise TypeError('rect_2 must be an instance of Rectangle')
+        area_1 = rect_1.area()
+        area_2 = rect_2.area()
+        if area_1 >= area_2:
+            return rect_1
+        elif area_2 > area_1:
+            return rect_2
