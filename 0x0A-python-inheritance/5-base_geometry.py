2acd202c591200f2662359eee29f3a67dc03f95e
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-12-05 00:20:18 +0400

    geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+This is module ''5-base_geometry'' provides one class, BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """This is an empty class that does not do anything until it's lastly
+    updated on 5th Dec, 12:18 am.
+
+    """
+    pass
