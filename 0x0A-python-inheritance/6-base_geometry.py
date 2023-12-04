015d8ab779cb806f8a340a4adcb33e03be913b6f
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-12-05 00:29:06 +0400

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+"""
+This is module ''5-base_geometry'' provides one class, BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """This is a class that does not do anything until it's lastly
+    updated on 5th Dec, 12:18 am.
+
+    5th Dec, 12:22 am: an area method is defined which also does nothing
+    but raising an exception with the message (area() is not implemented)
+
+    """
+    pass
+
+    def area(self):
+        """This function should calculate the area of the geometric shape in
+        the future. For now it only raises and exception of not being
+        implemented yet!
+        """
+        raise Exception('area() is not implemented')
