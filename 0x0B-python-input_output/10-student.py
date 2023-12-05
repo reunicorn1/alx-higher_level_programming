8f8ba756bd53f16720a414612a58e9d553913277
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-12-06 02:21:56 +0400

    student to json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+This module is '10-student' supplying class Student
+"""
+
+
+def class_to_json(obj):
+    """This function returns the dictionary description with simple data
+    structure (list, dictionary, string, integer and boolean) for JSON
+    serialization of an object.
+
+    It basically extracts information about attributes of an object
+    dynamically and creates a dictionary that represents its structure in
+    a JSON seriazable form.
+
+    Args:
+       obj (an instance of a class)
+
+    Returns:
+       a dictionary of class attributes.
+    """
+    return obj.__dict__
+
+
+class Student:
+    """This class defines a student.
+
+    Attributes:
+       first_name
+       last_name
+       age
+
+    Methods:
+       to_json(self)
+
+    The __init__ method initilizes a student by a first name, last name,
+    and an age.
+
+    Args:
+      first_name (str)
+      last_name (str)
+      age (int)
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """This function returns a dictionary of the object"""
+        dictionary = {}
+        note = class_to_json(self)
+        if attrs:
+            for attr in attrs:
+                try:
+                    dictionary[attr] = note[attr]
+                except KeyError:
+                    pass
+            return dictionary 
+        return note
