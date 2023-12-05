140742bcbe5424b77f39dabbc7f54712db46097c
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-12-06 01:52:00 +0400

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,7 +7,7 @@ This module is '8-class_to_json' and supplies function '8-class_to_json''
 def class_to_json(obj):
     """This function returns the dictionary description with simple data
     structure (list, dictionary, string, integer and boolean) for JSON
-    serialization of an object. -- 
+    serialization of an object.
 
     It basically extracts information about attributes of an object
     dynamically and creates a dictionary that represents its structure in
