25a5489ba033d774c0962b12ba4250d5f34fd716
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-12-05 23:33:13 +0400

    save object to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the '5-save_to_json_file' module, supplying one function 
+"""This is the '5-save_to_json_file' module, supplying one function
 ''save_to_json_file''
 """
 import json
