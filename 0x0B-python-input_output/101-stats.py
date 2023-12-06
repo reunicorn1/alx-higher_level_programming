3ca08c963d3a9419ae8d8979cd706afe1c1655ac
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-12-07 02:04:25 +0400

    log parse

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+This is a script which supplies multiple functions to log parse
+"""
+import sys
+
+
+total_size = 0
+code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+
+def log(status_code, size):
+    """This function logs data from each line of the stimulated HTTP
+    GET request for /project/260
+
+    Args:
+       status_code (str)
+       size (str)
+    """
+    global total_size
+    global code
+    total_size += int(size)
+    code[int(status_code)] += 1
+
+
+def flush():
+    """This function prints data existing into the stdout in a certain
+    format
+    """
+    global total_size
+    global code
+    print("File size: {}".format(total_size))
+    for key in sorted(code):
+        if code[key] == 0:
+            pass
+        else:
+            print("{}: {}".format(key, code[key]))
+
+
+n = 0
+try:
+    for line in sys.stdin:
+        size, status_code = line.split()[-1], line.split()[-2]
+        log(status_code, size)
+        n += 1
+        if (n % 10 == 0):
+            flush()
+except KeyboardInterrupt:
+    flush()
