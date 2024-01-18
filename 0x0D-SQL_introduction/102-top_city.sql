2969669c2313b2ea57dc9eaea8e6965d7ab611ba
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-01-18 21:02:16 +0400

    102-top_city.sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- This script displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month IN (7,8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
