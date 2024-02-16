3913109eda6a2f57330beea49d60fc5b88571633
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-02-16 20:42:56 +0400

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="127.0.0.1", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], database=sys.argv[3], port=3306)
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE BINARY name = '{}'\
