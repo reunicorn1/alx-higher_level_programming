c8b0e94e9cf739933bb003e6aee32ba4198f57f9
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-02-16 13:55:20 +0400

    2-my_filter_states.py case sensetive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], database=sys.argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '{}'  ORDER\
+    cursor.execute("SELECT * FROM states WHERE BINARY name = '{}'  ORDER\
             BY states.id".format(sys.argv[4]))
     rows = cursor.fetchall()
     for row in rows:
