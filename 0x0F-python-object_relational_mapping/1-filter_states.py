6e5d41df9842addefcdc073321e47952274cc107
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-02-16 13:30:54 +0400

    1-filter_states.py name without the name of the table prepended

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], database=sys.argv[3], port=3306)
     cursor = db.cursor()
-    qry = "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id"
+    qry = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"
     cursor.execute(qry)
     rows = cursor.fetchall()
     for row in rows:
