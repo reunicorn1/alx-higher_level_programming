bd2d8eec98567bc07b08bd0b830aba79fac67c85
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-02-16 23:29:51 +0400

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,8 +9,16 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
-class State(Base):
-    __tablename__ = "states"
-    id = Column(Integer(), primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
 
+class State(Base):
+    """
+    This is a class that identifies the table STATE using ORM
+
+    attributes or columns of the table are:
+      id (int)
+      name (str)
+    """
+    __tablename__ = "states"
+    id = Column(Integer(), primary_key=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
