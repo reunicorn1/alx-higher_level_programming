886db70362a49b290180f78ac678d9e4b63746f7
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-02-17 23:38:09 +0400

    101-relationship_states_cities_list.py

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+16. List relationship
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    rows = session.query(City).join(State).order_by(City.id, State.id).all()
+    state = None
+    for row in rows:
+        if row.state != state:
+            print("{}: {}".format(row.state.id, row.state.name))
+            state = row.state
+        print("\t {}: {}".format(row.id, row.name))
