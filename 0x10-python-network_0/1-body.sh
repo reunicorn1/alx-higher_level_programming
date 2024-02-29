7eb99dffe9afc01e946bfa1e34e7180646324555
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-02-29 23:58:36 +0400

    1-body.sh -in a nutshell

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # This script take a URL and sends GET request and displays the body of the response
-response=$(curl -sI -L "$1" | awk '/HTTP/{print $2}' | grep 200)
-if [ "$response" == "200" ]
-then
-        curl -sL "$1"
-fi
+curl -sL "$1"
