acf727c0727cc826c1065c82e877738bb4d5a5a8
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-01 04:36:00 +0400

    101-post_json.sh not json

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends JSON POST request and displays body of response
-curl -s --json @"$2" "$1"
+curl -s -d @"$2" --header "Content-Type: application/json" --header "Accept: application/json" "$1"
