921de2272b390663db6b87fe2ce9da0dc113bc92
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-02 00:25:07 +0400

    1-hbtn_header.py -get()

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-0. What's my status? #0
+1. Response header value #0"
 """
 from urllib import request
 import sys
 
 with request.urlopen(sys.argv[1]) as r:
-    print(r.headers.__getitem__("X-Request-Id"))
+    print(r.headers.get("X-Request-Id"))
