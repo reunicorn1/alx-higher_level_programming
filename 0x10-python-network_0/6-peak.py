56e8381c08ff2597d73de61adf860d406e901c13
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-01 06:39:47 +0400

    6-peak.py pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,19 +3,20 @@
 6. Find a peak
 """
 
-def find_peak(list_of_integers):
+
+def find_peak(array):
     """
     This function finds a peak in a list of unsorted integer
     """
-    if not list_of_integers or not len(list_of_integers):
+    if not array or not len(array):
         return None
     lo = 0
-    hi = len(list_of_integers) - 1
+    hi = len(array) - 1
     while hi >= lo:
         mid = lo + ((hi - lo) // 2)
-        if mid < len(list_of_integers) - 1 and list_of_integers[mid + 1] >= list_of_integers[mid]:
+        if mid < len(array) - 1 and array[mid + 1] >= array[mid]:
             lo = mid + 1
-        elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
+        elif mid > 0 and array[mid - 1] > array[mid]:
             hi = mid - 1
         else:
-            return list_of_integers[mid]
+            return array[mid]
