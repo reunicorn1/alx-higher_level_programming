2a190db0ff3243e0f3b888c0704101874f593269
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-02 04:08:16 +0400

    8-json_api.py json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     data = {'q': ""}
     if len(sys.argv) > 1:
         data['q'] = sys.argv[1]
-    r = requests.post('http://0.0.0.0:5000/search_user', data=data)
+    r = requests.post('http://0.0.0.0:5000', data=data)
     try:
         if not r.json():
             print("No result")
         else:
             print("[{}] {}".format(r.json()['id'], r.json()['name']))
-    except Exception as e:
+    except requests.exceptions.JSONDecodeError:
         print("Not a valid JSON ")
