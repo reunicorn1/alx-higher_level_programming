137599ee01e6a9b9c9a349ada5af90c09e175682
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-02 04:10:03 +0400

    8-json_api.py json1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     data = {'q': ""}
     if len(sys.argv) > 1:
         data['q'] = sys.argv[1]
-    r = requests.post('http://0.0.0.0:5000', data=data)
+    r = requests.post('http://0.0.0.0:5000/search_user', data=data)
     try:
         if not r.json():
             print("No result")
