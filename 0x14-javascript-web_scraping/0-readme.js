b8c1c607c8c190e95d9463722babdc67a45e7751
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-27 01:47:09 +0400

    removing string literal

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 /* The required modules have been established here */
 const fs = require('fs');
 
-fs.readFile(`./${process.argv[2]}`, 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
