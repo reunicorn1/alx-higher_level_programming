7dfad875cc3f355c68bacb86ab513ba5d729a52f
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-27 01:59:10 +0400

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* The required modules have been established here */
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
